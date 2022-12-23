8c02f17bcc26ae9bfd46c7182ef4875c22ce0808
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-23 13:37:08 +0530

    Added:Logic for Displaying  the Junit welcome meassage

---
+++ src/main/java/com/java/junitprograms/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.java.junitprograms;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the User Registration program");
+	}
+}
