224002ace19c8f53571ed62080d18816c2de858f
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-23 19:39:46 +0530

    Added:Logic for Testing the User entry for using Junit Test

--- src/main/java/com/java/junitprograms/UserRegistration.java
+++ src/main/java/com/java/junitprograms/UserRegistration.java
@@ -1,7 +1,41 @@
 package com.java.junitprograms;
 
-public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the User Registration program");
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration { 
+	public  boolean firstName(String firstName) {
+		String regex ="^[A-Z]{1,}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(firstName);
+		return matcher.matches();
 	}
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1,}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(lastName);
+		return matcher.matches();
+	}
+	public boolean emailId(String emailId) {
+		String regex = "^[a-z]+[.][a-zA-Z]+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-zA-Z]{2,3})*$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(emailId);
+		return matcher.matches();
+		
+	}
+	public boolean mobileNumber(String mobileNumber) {
+		String regex ="^(91)[ ]{1}[6-9]{1}[0-9]{9}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(mobileNumber) ;
+		return matcher.matches();
+		
+	}
+	public boolean passWord(String password) {
+		String regex = "((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$-%^&*]).{8,})";
+		Pattern pattern = Pattern .compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+	
+	}	
+	
 }
--- src/test/java/com/java/junitprograms/UserRegistrationTest.java
+++ src/test/java/com/java/junitprograms/UserRegistrationTest.java
@@ -1,5 +1,88 @@
 package com.java.junitprograms;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+	UserRegistration regitration = new UserRegistration();
+	/*
+	 * Testing for FirstName using happy test and sad test methods
+	 * HappyTest case Validate the Entry successfully
+	 * Sadtest case Fails the Entry
+	 */
+	@Test
+	public void happyTestFirstName()
+	{
+		boolean result = regitration.firstName("Shashikant");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void sadTestFirstName()
+	{
+		boolean result = regitration.firstName("kant");
+		Assert.assertEquals(false, result);
+	}
+	/*
+	 * Testing for LastName using happy test and sad test methods
+	 * HappyTest case Validate the Entry successfully
+	 * Sadtest case Fails the Entry
+	 */
+	@Test
+	public void happyTestLastName() {
+		boolean result =regitration.lastName("Daddimani");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void sadTestLastNmae() {
+		boolean result =regitration.lastName("daddimani");
+		Assert.assertEquals(false, result);
+	}
+	/*
+	 * Testing for EmailId using happy test and sad test methods
+	 * HappyTest case Validate the Entry successfully
+	 * Sadtest case Fails the Entry
+	 */
+	@Test
+	public void happyTestEmailId() {
+		boolean result = regitration .emailId("abc.xyz@bl.co.in");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void sadTestEmailId() {
+		boolean result = regitration.emailId("Abc.1xy@.bl.src.in");
+		Assert.assertEquals(false, result);
+	}
+	/*
+	 * Testing for MobileNumber using Happy test and Sad test methods
+	 * HappyTest case Validate the Entry successfully
+	 * Sadtest case Fails the Entry
+	 */
+	@Test
+	public void happyTestMobileNumber() {
+		boolean result = regitration.mobileNumber("91 9113264303");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void sadTestMobileNumber() {
+		boolean result = regitration.mobileNumber("91 5113264303");
+		Assert.assertEquals(false, result);
+   }
+	/*
+	 * Testing for Password using happy test and sad test methods
+	 * HappyTest case Validate the Entry successfully
+	 * Sadtest case Fails the Entry
+	 */
+	@Test
+    public void happyTestPassword(){
+	 boolean result = regitration.passWord("Sdaddi@12");
+	 Assert.assertEquals(true, result);
+    }
+	@Test
+ 	public void sadTestPassword(){
+		boolean result = regitration.passWord("Sdaddi12");
+		Assert.assertEquals(false, result);
+	}
 
 }
